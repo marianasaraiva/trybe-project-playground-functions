c29bda203fa1f6f0466c62946df94a5345caf1a6
Mariana Saraiva <marianascm8@gmail.com>
2021-09-02 09:47:16 -0300

    Requisito 10

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,6 +1,21 @@
 // Desafio 10
-function techList() {
-  // seu código aqui
+function techList(tech, name) {
+  tech.sort(); 
+  let arr = [];
+  // for para trazer cada elemento do array
+  for (let index = 0; index < tech.length; index += 1) {
+   //montando o array de objetos para dinamico 
+    arr.push({
+      tech: tech[index], 
+      name: name
+      }) 
+  }
+  //condicional para relatar erro 
+  if (arr.length === 0) {
+    return "Vazio!"
+  } else {
+    return arr;
+  }
 }
 
 // Desafio 11
